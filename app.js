7e4196f89818bb5c5ea683626210d3efdab7c928
gureuso <wyun13043@gmail.com>
2020-01-02 16:01:10 +0900

    Change error system

--- app.js
+++ app.js
@@ -27,15 +27,12 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({'message': err.message, 'code': err.status * 100});
 });
 
-app.listen(3000, () => console.log(`app listening on port 3000!`));
+app.listen(3000, () => {
+  console.log(`app listening on port 3000!`)
+});
 
 //module.exports = app;
